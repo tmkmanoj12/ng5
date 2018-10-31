ad4c34a108e4aab8225ec262e0f66ad2d3424bfd
manoj <manoj.t@transneuron.com>
2018-11-01 04:00:38 +0530

    chenged impoort

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {trigger,style ,transition,animate,keyframes,query,stagger} from '@angular/animations';
-import {DataService} from '../data.service'
-import request  = require('request')
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
+import { DataService } from '../data.service'
+import * as request from 'request'
 
 @Component({
   selector: 'app-home',
@@ -12,21 +12,21 @@ import request  = require('request')
     trigger('goals', [
       transition('* => *', [
 
-        query(':enter', style({ opacity: 0 }), {optional: true}),
+        query(':enter', style({ opacity: 0 }), { optional: true }),
 
         query(':enter', stagger('300ms', [
           animate('.6s ease-in', keyframes([
-            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
-            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
-          ]))]), {optional: true})
-          ,
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35px)', offset: 0.3 }),
+            style({ opacity: 1, transform: 'translateY(0)', offset: 1.0 }),
+          ]))]), { optional: true })
+        ,
         query(':leave', stagger('300ms', [
           animate('.6s ease-out', keyframes([
-            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
-            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
-            style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
-          ]))]), {optional: true})
+            style({ opacity: 1, transform: 'translateY(0)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35px)', offset: 0.3 }),
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 1.0 }),
+          ]))]), { optional: true })
       ])
     ])
 
@@ -34,19 +34,19 @@ import request  = require('request')
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private _data :   DataService) { }
-  itemCount : number = 10
-  btnText : string = "Please add item"
-  goalText : string = "My first Goal"
+  constructor(private _data: DataService) { }
+  itemCount: number = 10
+  btnText: string = "Please add item"
+  goalText: string = "My first Goal"
   goals = []
   ngOnInit() {
-    
-    this._data.goal.subscribe(res => {this.goals = res});
+
+    this._data.goal.subscribe(res => { this.goals = res });
     this.itemCount = this.goals.length
     this._data.changeGoal(this.goals)
   }
   addItem() {
-    request.get('https://jsonplaceholder.typicode.com/todos/2',function(err,response,body){
+    request.get('https://jsonplaceholder.typicode.com/todos/2', function (err, response, body) {
       alert(body)
     })
     this.goals.push(this.goalText)
@@ -55,8 +55,8 @@ export class HomeComponent implements OnInit {
     this._data.changeGoal(this.goals)
 
   }
-  removeItem(i){
-    this.goals.splice(i,1)
+  removeItem(i) {
+    this.goals.splice(i, 1)
     this._data.changeGoal(this.goals)
   }
 
